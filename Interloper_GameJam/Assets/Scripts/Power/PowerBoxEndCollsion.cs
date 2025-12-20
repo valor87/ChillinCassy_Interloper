f753f7b5157ea13e1bdc9cb94fbec328f6641b9d
valor87 <134859690+valor87@users.noreply.github.com>
2025-12-20 18:26:04 -0500

    Startng the flood lights

--- Interloper_GameJam/Assets/Scripts/Power/PowerBoxEndCollsion.cs
+++ Interloper_GameJam/Assets/Scripts/Power/PowerBoxEndCollsion.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 using System.Collections.Generic;
 public class PowerBoxEndCollsion : MonoBehaviour
 {
+    public Transform PowerBox;
+    EventCore EventCore;
     public PowerBoxScript PowerBoxScript;
     public List<Vector3> RandomPos = new List<Vector3>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-   
+    private void Start()
+    {
+        EventCore.enableFog.AddListener(SetPos);
+    }
     private void OnTriggerEnter(Collider other)
     {
         print("You win");
         PowerBoxScript.runrepair = false;
     }
-    public void SetPos(Transform ParentTransform)
+    public void SetPos(string Cause)
     {
-        int num = Random.Range(0, RandomPos.Count);
-        transform.localPosition = RandomPos[num];
+        if (Cause == "power")
+        {
+            this.gameObject.GetComponent<BoxCollider>().enabled = true;
+            int num = Random.Range(0, RandomPos.Count);
+            transform.localPosition = RandomPos[num];
+        }
     }
 }
